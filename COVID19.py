405ff12d03acc37165eee7a2d3cb2cb7502cc552
NotSharwan <sharwansolanki01@gmail.com>
2020-04-14 15:32:22 +0530

    Made some Major Changes _-_

--- COVID19.py
+++ COVID19.py
@@ -14,42 +14,22 @@ soup = BeautifulSoup(URL, 'html.parser')
 DATA = ""
 
 # Dictionary of states!
-states = {0: 'Total Number Of Cases In India',
-          1: 'Andhra Pradesh',
-          2: 'Andaman and Nicobar Islands',
-          3: 'Arunachal Pradesh',
-          4: 'Assam',
-          5: 'Bihar',
-          6: 'Chandigarh',
-          7: 'Chhattisgarh',
-          8: 'Delhi',
-          9: 'Goa',
-          10: 'Gujarat',
-          11: 'Haryana',
-          12: 'Himachal Pradesh',
-          13: 'Jammu and Kashmir',
-          14: 'Jharkhand',
-          15: 'Karnataka',
-          16: 'Kerala',
-          17: 'Ladakh',
-          18: 'Madhya Pradesh',
-          19: 'Maharashtra',
-          20: 'Manipur',
-          21: 'Mizoram',
-          22: 'Odisha',
-          23: 'Puducherry',
-          24: 'Punjab',
-          25: 'Rajasthan',
-          26: 'Tamil Nadu',
-          27: 'Telengana',
-          28: 'Tripura',
-          29: 'Uttarakhand',
-          30: 'Uttar Pradesh',
-          31: 'West Bengal'}
+states = {0: 'Total Number Of Cases In India'}
+
+# To get State Data
+stateList = []
+for tr in soup.find_all('tbody')[0].find_all('tr'):
+    sL = tr.get_text()[1:]
+    sList = sL.split("\n")[:2]
+    stateList.append(sList)
+stateList = stateList[:-2]
+for state in stateList:
+    states[int(state[0])] = ' '.join(list(state[1].strip().split()))
+print()
 print("-----------------------------------------------------------")
 print("Choose a Value [State / UT]:")
 print("---------------")
-for keys, values in sorted(states.items()):
+for keys, values in states.items():
     print(keys, ":", values)
 print("-----------------------------------------------------------")
 print('ENTER "STOP" to stop the Execution!')
@@ -67,7 +47,7 @@ while (True):
         break
     else:
         selectedState = int(selectedState)
-        if selectedState < 0 or selectedState > 31:
+        if selectedState < 0 or selectedState > (len(states)-1):
             print("Invalid Input: 'Enter Valid State Value'")
             break
     s = 0
